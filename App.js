54c6ebba53eb0383be400f4920907ff46fde2963
Tamojit BISWAS <43926983+tamojitbiswas@users.noreply.github.com>
2019-01-03 19:20:48 +0530

    Login Page Updated

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import {View} from 'react-native';
 import Flash_Screen from './FlashScreen';
 import Footer_Component from './Footer';
 import Header_Component from './Header';
+import Log_In from './Login';
 export default class App extends Component {
   
   render() {
     return (
       <View style={{ flex: 1 }}>
         <Header_Component/>
-        <Flash_Screen />        
+        
+        <Log_In/>        
         <Footer_Component/>
       </View>
     );
