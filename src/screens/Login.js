f93918d1c4ce808f8cd9640b56c125f80ad4ab16
Tamojit BISWAS <43926983+tamojitbiswas@users.noreply.github.com>
2019-01-04 15:26:28 +0530

    Login Screen External CSS updation

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,103 +1,125 @@
-import React, { Component } from 'react';
-import { View, Image, Text, Alert, TouchableOpacity, TextInput, CheckBox, Button } from 'react-native';
-import {LoginStyles} from '../styelsheets/MainStyle';
+        import React, { Component } from 'react';
+        import { View, Image, Text, Alert, TouchableOpacity, TextInput, CheckBox, Button, ScrollView } from 'react-native';
+        import {LoginStyles} from '../styelsheets/MainStyle';
+//import { ScrollView } from 'react-native-gesture-handler';
 
-export default class LogIn extends Component {    
-    constructor(props) {
-        super(props);
-        //this.state = { text: '' };
-        console.log(this)
-    }
-    
-    render() {
-        return (
-            <View style={LoginStyles.mainWrapper}>
-                <View style={LoginStyles.bannerArea}>
-                    <View style={{ flex: 0.1, alignContent:'center', paddingTop:5}}>
-                        <TouchableOpacity onPress={() => console.log('back')}>
-                            <Image style={{ width:20, height:20}}
-                                source={require('../../assets/images/back.png')}
-                                />
-                        </TouchableOpacity>
-                    </View>
-                        
-                    <View style={{ flex:1, paddingLeft:100 }}>
-                        <Text style={{ fontSize: 24, color:'#616264'}}> MEDePAL</Text>
-                    </View>
-                </View>
-                <View style={LoginStyles.LoginForm}>
-                    <View style={{ flex: 0.5, alignItems:'center', paddingTop:7 }}>
-                        <Text>Member Sign IN</Text>
-                    </View>
-                    <View style={{ flex: 0.8, paddingLeft: 10, paddingRight: 10, paddingTop: 15  }}>
-                        <Text>Email/Mobile number</Text>
-                        <TextInput style={{ height: 30, borderBottomColor: 'black', borderBottomWidth: 1 }}
-                            placeholder="Type your Email/Mobile"
-                            onChangeText={text => this.setState({ text })}
-                        />
-                    </View>
-                    <View style={{ flex: 0.8, paddingLeft: 10, paddingRight: 10, paddingTop:15, paddingBottom:5 }}>
-                        <Text>Password</Text>
-                        <TextInput
-                            style={{ height: 30, borderBottomColor: 'black', borderBottomWidth: 1 }}
-                            placeholder="Type your Password"
-                            secureTextEntry={true}
-                        />
-                    </View>
-                    <View style={{ flex: 0.6, flexDirection:'row',}}>
-                        <View style={{ flex: 1,paddingLeft: 10, paddingRight: 10, flexDirection:'row'}}>
-                        <View style={{flex:0.3, paddingTop:3}}>
-                        <CheckBox
-                            //title="Remember me"
+        export default class LogIn extends Component {    
+            constructor(props) {
+                super(props);
+                //this.state = { text: '' };
+                console.log(this)
+            }
+            
+            render() {
+                return (
+                    
+                    <View style={LoginStyles.mainWrapper}>
+                    <ScrollView>
+                        <View style={LoginStyles.bannerArea}>
+                            <View style={LoginStyles.bannerArea_Button}>
+                                <TouchableOpacity onPress={() => console.log('back')}>
+                                    <Image style={{ width:20, height:20,}}
+                                        source={require('../../assets/images/back.png')}
+                                        />
+                                </TouchableOpacity>
+                            </View>
+                                
+                            <View style={LoginStyles.bannerArea_Text}>
+                                <Text style={{ fontSize: 24, color:'#616264',}}> MEDePAL</Text>
+                            </View>
+                        </View>
+                        <View style={LoginStyles.bannerArea2_Text}>
+                            <Text>
+                                Member Sign IN
+                                    </Text>
+                        </View>
+
+                        <View style={LoginStyles.textInput}>
+                            <Text>
+                                Email/Mobile number
+                                    </Text>
+                            <TextInput
+                                style={{ height: 25, borderBottomColor: 'black', borderBottomWidth: 1, }}
+                                placeholder="Type your Email/Mobile"
+                                onChangeText={text => this.setState({ text })}
+
+                            />
+
+                        </View>
+
+                    <View style={LoginStyles.textInput}>
+                            <Text>
+                                Password
+                                    </Text>
+                            <TextInput
+                                style={{ height: 25, borderBottomColor: 'black', borderBottomWidth: 1, }}
+                                placeholder="Type your Password"
+                                secureTextEntry={true}
                             />
                         </View>
-                        <View style={{flex:1, paddingTop:9,}}>
-                            <Text>Remember me</Text>
+
+                            <View style={LoginStyles.checkBox_Main_Container1}>
+                                <View style={LoginStyles.checkBox_Secondary_Container1}>
+                                    <View style={LoginStyles.checkBox_Secondary_Container_Row1}>
+                                    <CheckBox
+                                    //title="Remember me"
+                                    />
+                                </View>
+                                    <View style={LoginStyles.checkBox_Secondary_Container_Row2}>
+                                    <Text>Remember me</Text>
+                                </View>
+                            </View>
+                                <View style={LoginStyles.toggleButton_Main_Container}>
+                                <View style={LoginStyles.toggleButton_Sub_Container}>
+                                    <View style={{ flex: 1, backgroundColor: '#616264', alignItems: 'center', }}>
+                                        <TouchableOpacity onPress={() => console.log('Password')}>
+                                            <Text>
+                                                Password
+                                        </Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                    <View style={{ flex: 0.5, backgroundColor: 'white', alignItems: 'center', }}>
+                                        <TouchableOpacity onPress={() => console.log('OTP')}>
+                                            <Text>
+                                                OTP
+                                        </Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            </View>
                         </View>
-                        </View>
-                        <View style={{ flex: 0.5,  paddingLeft: 10, paddingRight: 10,}}>
-                        <View style={{flex:1, paddingTop: 8, paddingBottom: 10, flexDirection:'row'}}>
-                            <View style={{ flex: 1, backgroundColor:'#616264',alignItems:'center' }}>
-                                <TouchableOpacity onPress={() => console.log('Password')}>    
-                                <Text>
-                                Password
-                                </Text>
+
+                            <View style={LoginStyles.button}>
+                            <View style={{ flex: 1, }}>
+                            </View>
+                            <View style={{ flex: 1 }}>
+                                <Button
+                                    onPress={() => console.log('Sign In Button')}
+                                    title="Sign In"
+                                    color="#616264"
+                                    width="10"
+                                />
+                                <TouchableOpacity onPress={() => console.log('Forgot Password')}>
+                                    <Text style={{ textDecorationLine: 'underline' }}>Forgot password ?</Text>
                                 </TouchableOpacity>
                             </View>
-                            <View style={{flex:0.5, backgroundColor:'white', alignItems:'center'}}>
-                                <TouchableOpacity onPress={() => console.log('OTP')}>    
-                                <Text>
-                                OTP
-                                </Text>
-                                </TouchableOpacity>
+                            <View style={{ flex: 1, }}>
                             </View>
+
                         </View>
-                        </View>
-                    </View>
-                    <View style={{ flex: 1, justifyContent: 'center', flexDirection:'row' }}>
-                        <View style={{flex:1,}}></View>
-                        <View style={{ flex: 1 }}>
-                            <Button onPress={() => console.log('Sign In Button')} title="Sign In" color="#616264" width="10" />
-                            <TouchableOpacity onPress={() => console.log('Forgot Password')}>
-                                <Text style={{textDecorationLine:'underline'}}>Forgot password ?</Text>
-                            </TouchableOpacity>
-                        </View>   
+
+                        <View style={{flex:1}}></View>
+
+                        
+
+                        </ScrollView>
                     </View>
 
-                </View>
-                <View>
-
-                </View>
-                <View>
-
-                </View>
-            </View>
 
 
 
 
-
-            );
-        }
-    }
+                    );
+                }
+            }
 
--- src/styelsheets/MainStyle.js
+++ src/styelsheets/MainStyle.js
@@ -1,46 +1,118 @@
-import {StyleSheet} from 'react-native';
+  import {StyleSheet} from 'react-native';
 
-export const LayoutStyles = StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-});
-
-export const HeaderStyles = StyleSheet.create({
-
-});
-
-export const FooterStyles = StyleSheet.create({
-
-});
-
-export const MainStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-export const ButtonStyles = StyleSheet.create({
-
-});
-
-export const LoginStyles = StyleSheet.create({
-  mainWrapper: {
+  export const LayoutStyles = StyleSheet.create({
+    container:{
       flex: 1,
-      justifyContent: 'center',
-      backgroundColor: 'red',
+    },
+  });
 
-  },
-  bannerArea: {
-    flex: 0.7,
-    flexDirection: 'row',
-    borderBottomColor: 'black',
-    borderBottomWidth: 1
-  }
-});
+  export const HeaderStyles = StyleSheet.create({
+
+  });
+
+  export const FooterStyles = StyleSheet.create({
+
+  });
+
+  export const MainStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  export const ButtonStyles = StyleSheet.create({
+
+  });
+
+  export const LoginStyles = StyleSheet.create({
+    mainWrapper: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+    },
+    bannerArea: {
+      flex: 0.3,
+      flexDirection: 'row',
+      borderBottomColor: 'black',
+      borderBottomWidth: 1
+    },
+    bannerArea_Button:{
+      flex: 0.05, 
+      alignContent: 'center', 
+      paddingTop: 5,
+      //backgroundColor:'black',
+    },
+    bannerArea_Text:{
+      flex: 0.5, 
+      paddingLeft: 100
+    },
+    bannerArea2_Text:{
+      flex: 0.2, 
+      alignItems: 'center', 
+      paddingTop: 7, 
+    },
+    textInput:{
+      flex: 0.5, 
+      paddingLeft: 10, 
+      paddingRight: 10, 
+      paddingTop: 10, 
+      paddingBottom: 1,
+    },
+    checkBox_Main_Container1:{
+      flex: 0.3, 
+      flexDirection: 'row', 
+    },
+    checkBox_Secondary_Container1:{
+      flex: 1, 
+      paddingLeft: 10, 
+      paddingRight: 10, 
+      flexDirection: 'row',
+    },
+    checkBox_Secondary_Container_Row1:{
+      flex: 0.3, 
+      paddingTop: 3, 
+    },
+    checkBox_Secondary_Container_Row2:{
+      flex: 1, 
+      paddingTop: 9,
+    },
+    toggleButton_Main_Container:{
+      flex: 0.5, 
+      paddingLeft: 10, 
+      paddingRight: 10,
+    },
+    toggleButton_Sub_Container:{
+      flex: 1, 
+      paddingTop: 8, 
+      paddingBottom: 10, 
+      flexDirection: 'row',
+    },
+    toggleButton_Sub_Container_Row1: {
+      flex: 1, 
+      backgroundColor: '#616264', 
+      alignItems: 'center',
+    },
+    toggleButton_Sub_Container_Row2: {
+      flex: 0.5, 
+      backgroundColor: 'white', 
+      alignItems: 'center',
+    },
+    button: {
+      flex: 0.4,
+      justifyContent: 'center',
+      flexDirection: 'row',
+    },
+  });
+
+  export const SignIn = StyleSheet.create({
+
+    button:{
+      flex: 0.4, 
+      justifyContent: 'center', 
+      flexDirection: 'row',
+    },
+
+  });
