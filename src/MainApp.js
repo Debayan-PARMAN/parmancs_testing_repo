b6272c06fc7919f69295df61dd5b44003f802fa6
DebayanSen7 <debayan.sen7@gmail.com>
2019-01-12 08:49:59 +0530

    Updated ROUTES, User-Reducer

--- src/MainApp.js
+++ src/MainApp.js
@@ -5,17 +5,15 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import {LayoutStyles} from './styelsheets/MainStyle';
 import Footer from './components/Footer/Footer';
 
-import Login from './screens/Login';
-import Registration from './screens/Registration';
-import Home from './screens/Home';
-import FlashScreen from './screens/FlashScreen';
+import ROUTES from '../src/constants/route';
+
 import CreateAccount_Btn from './components/Button/CreateAccount_Button';
 
 const RootStack = createStackNavigator({
-  Home: Home,
-  Loading: FlashScreen,
-  Login: Login,
-  Registration: Registration,
+  Home: ROUTES.HOME,
+  Loading: ROUTES.LOADING,
+  Login: ROUTES.LOGIN,
+  Registration: ROUTES.REGISTRATION,
   Registration: CreateAccount_Btn,
 
 
